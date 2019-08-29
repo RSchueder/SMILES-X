ea1ccddd8679faf1ff8d59173cd2941e7d8849f1
RSchueder <rudy.schueder@outlook.com>
2019-08-29 09:38:05 +0200

    First instance of model working with SOLUTIONS substances
    Model was run with SOLUTIONS substances, database extractor was added to produce the input data

--- data_query.py
+++ data_query.py
@@ -1,4 +1,12 @@
-import prop_database as db
-import pandas as pd 
+'''
+create data table based on query from substance property database
+'''
 
-substances = db.substance_list()
+from produce_data import main
+
+prop = 'log Kow'
+df = main(prop)
+df.dropna(axis = 0, inplace = True)
+df = df[['SMILES', 'value']]
+df.rename(columns = {'SMILES': 'smiles', 'value' : prop})
+df.to_csv(r'validation_data\SOLUTIONS_%s.csv' % prop)
